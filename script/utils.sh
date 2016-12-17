2f7f0342da669baf81e5e7014aa7a91d4c1228ee
Jakub Jirutka <jakub@jirutka.cz>
2016-12-18 00:32:16 +0100

    Allow to build packages even on untagged commit

--- script/utils.sh
+++ script/utils.sh
@@ -23,6 +23,23 @@ ewarn() {
 	printf '\033[1;33m> %s\033[0m\n' "$@" >&2
 }
 
+# Resolves project version based on v* tags in git repository. If HEAD is not
+# tagged (i.e. this is not a release), then it returns last version with
+# suffix -dev.
+repo_version() {
+	local desc
+
+	# If git HEAD is tagged as v*, then it prints name of the tag.
+	if desc="$(git describe --tags --exact-match --match 'v*' 2>/dev/null)"; then
+		echo "${desc#v}"
+	# Prints name of the last v* tag in the tree with suffix `-<n>-<abbrev>`.
+	elif desc="$(git describe --tags --match 'v*' 2>/dev/null)"; then
+		echo "$desc" | sed -En 's/^v([^-]+).*/\1-dev/p'
+	else
+		return 1
+	fi
+}
+
 has() {
 	command -v "$1" >/dev/null 2>&1
 }
