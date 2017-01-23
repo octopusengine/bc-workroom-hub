3a88f5c27ee68983651a289d5a7019dc4a6b03be
Jakub Jirutka <jakub.jirutka@bigclown.com>
2017-01-23 14:02:31 +0100

    Fix build-deb to be compliant with Debian version format

--- script/utils.sh
+++ script/utils.sh
@@ -23,18 +23,21 @@ ewarn() {
 	printf '\033[1;33m> %s\033[0m\n' "$@" >&2
 }
 
-# Resolves project version based on v* tags in git repository. If HEAD is not
-# tagged (i.e. this is not a release), then it returns last version with
-# suffix -dev.
-repo_version() {
+# Infers project version in Debian format from v* tags in git repository.
+# Underscores (_) and hyphens (-) are replaced with tilde (~) - the suffix
+# separator in Debian version number format.
+# If HEAD is not tagged (i.e. this is not a release), then it returns last
+# version with suffix ~dev.
+pkgver_from_git() {
 	local desc
 
 	# If git HEAD is tagged as v*, then it prints name of the tag.
 	if desc="$(git describe --tags --exact-match --match 'v*' 2>/dev/null)"; then
-		echo "${desc#v}"
+		echo "${desc#v}" | tr '[_-]' '~'
 	# Prints name of the last v* tag in the tree with suffix `-<n>-<abbrev>`.
 	elif desc="$(git describe --tags --match 'v*' 2>/dev/null)"; then
-		echo "$desc" | sed -En 's/^v([^-]+).*/\1-dev/p'
+		# Replace git suffix with ~dev.
+		echo "$desc" | sed -En 's/^v([^-]+).*/\1~dev/p'
 	else
 		return 1
 	fi
